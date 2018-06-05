fd2532cb9e55f869efbb2505c35fb1a20d4bf1a3
Pavel <z123370@rambler.ru>
2018-06-05 14:50:37 +0300

    решённая задача 1

---
+++ task1.rb
@@ -0,0 +1,11 @@
+﻿
+h= ARGV[0]
+l=h.to_s
+m=h.encode(Encoding::UTF_8)
+z1= m.gsub(/Р/, '')
+z2=z1.gsub(/р/, '')
+z3=z2.gsub(/К/, '')
+z4=z3.gsub(/к/, '')
+z5=z4.gsub(/Н/, '')
+z6=z5.gsub(/н/, '')
+puts z6
